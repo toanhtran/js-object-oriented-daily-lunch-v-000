5b8faa6d6e5ecb980bfc51694ba08272eb7e93d8
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-14 02:25:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-class Neighborhood(){
-  
-}
+
+const Neighborhood =(() => {
+  let neighborhoodIds = 1;
+  return class {
+    constructor(name) {
+      this.id = neighborhoodIds++;
+      this.name = name;
+      store.neighborhoods.push(this);
+    }
+
+    customers() {
+      return store.customers.filter(customer => customer.neighborhoodId === this.id);
+    }
+
+    meals() {
+      const allMeals = this.customers().map(customer => customer.meals());
+      const merged = [].concat.apply([], allMeals);
+      return [...new Set(merged)]''
+    }
+
+    deliveries(){
+      return store.deliveries.filter(delivery => delivery.neighborhoodId === this.id);
+    }
+  };
+})();
